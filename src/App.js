0604346a7a75f3caf0eb8038da5b2e708dfb340b
beke83 <bekebenjamin@yahoo.com>
2023-04-08 03:23:15 +0100

    Product card prview in progress

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="product">
+      <div className="product-container">
+        <div className='product-image-container'>
+          <img src='./assets/images/image-product-desktop.jpg' className="product-image" alt="" />
+        </div>
+        <div className='product-desc'>
+          <p>Perfume</p>
+          <h3>Gabrielle Essence Eau De Parfum</h3>
+          <h6>A floral, solar and voluptuous interpretation composed by Olivier, Polge, Perfumer-Creator for the House of CHANEL</h6>
+          <h4>$149.99
+            <span>$16.99</span>
+          </h4>
+          <button className='add-btn'>Add to cart</button>
+        </div>
+      </div>
     </div>
   );
 }
