fd32dba54d22800698daaa2642cd689d184d7891
beke83 <bekebenjamin@yahoo.com>
2023-04-12 01:57:21 +0100

    Desktop view done

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ function App() {
           <h3>Gabrielle Essence Eau De Parfum</h3>
           <h6>A floral, solar and voluptuous interpretation composed by Olivier, Polge, Perfumer-Creator for the House of CHANEL</h6>
           <h4>$149.99
-            <span>$16.99</span>
+            <span>$169.99</span>
           </h4>
-          <button className='add-btn'>Add to cart</button>
+          <button className='add-btn'>
+            <span style={{marginRight: '10px'}}><img src='/assets/images/icon-cart.svg' alt="cart"/></span>Add to cart</button>
         </div>
       </div>
     </div>
